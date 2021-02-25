44a38bf46fc10f4dc5017cb6d64936c9de43ae88
Panda <dxz6@mail.ru>
2021-02-25 23:14:22 +0300

    Primes

--- src/Primes.java
+++ src/Primes.java
@@ -1,15 +1,15 @@
 public class Primes{
 	public static void main(String[] args) {
-		for (int num = 2; num <= 100; num++) {
-			if (isPrime(num) == true)
+		for (int num = 2; num <= 100; num++) { //Генерация цифр и их проверка
+			if (isPrime(num) == true)//Вывод простых чисел 
 				System.out.println(num);
 		}
 
 	}
-	public static boolean isPrime(int num) {
-		for (int i=2; i < num; i++)
+	public static boolean isPrime(int num) { //Метод проверки 
+		for (int i=2; i < num; i++) //Цикл проверки числа
 		{
-				if (num % i == 0) {
+				if (num % i == 0) { //Условие простых чисел
 					return false;
 				}
 		}
