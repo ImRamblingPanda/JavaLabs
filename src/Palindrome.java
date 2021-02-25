314d46083633a904c9eda12ea1a3520710da79a0
Panda <dxz6@mail.ru>
2021-02-25 23:10:21 +0300

    C2

---
+++ src/Palindrome.java
@@ -0,0 +1,21 @@
+public class Palindrome {
+ public static void main(String[] args) {
+	 String[] words = {"java", "Palindrome","madam","racecar","apple","kayak","song","noon"}; 
+ for (int i = 0; i < words.length; i++) {
+	 if (isPalindrome(words[i]) == true) {
+		 System.out.println(words[i] + " - Является Палиндромом");
+	 }
+	 else System.out.println(words[i] + " - Не является Палиндромом");
+}
+ }
+public static boolean isPalindrome(String s) {
+	String palindrome = "";
+	for (int i = s.length() - 1; i >= 0; i--) {
+		palindrome = palindrome + s.charAt(i);
+	}
+	if (palindrome.equals(s)){
+		return true;
+	}
+	else return false;
+	}
+}
---
+++ src/Primes.java
@@ -0,0 +1,18 @@
+public class Primes{
+	public static void main(String[] args) {
+		for (int num = 2; num <= 100; num++) {
+			if (isPrime(num) == true)
+				System.out.println(num);
+		}
+
+	}
+	public static boolean isPrime(int num) {
+		for (int i=2; i < num; i++)
+		{
+				if (num % i == 0) {
+					return false;
+				}
+		}
+		return true;
+	}
+}
